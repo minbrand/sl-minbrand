52b22c22d85dc73bfbbf43eb7d53c45b3713c6c8
jotainvest <108202340+jotainvest@users.noreply.github.com>
2024-12-07 20:45:55 -0300

    Add files via upload

---
+++ upxpto-anual01/js/script.js
@@ -0,0 +1,9 @@
+// Função chamada quando o usuário clica no botão de rejeitar
+function rejeitar() {
+    window.location.href = "https://lastlink.com/p/C264BD25A/checkout-payment"; // Redireciona para a página de sinal
+}
+
+// Função chamada quando o usuário clica no botão de compra
+function comprar() {
+    window.location.href = "https://lastlink.com/p/C39126E40/checkout-payment/"; // Redireciona para a página de sinal
+}
